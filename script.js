6e359c47a02dd65bc9cc71dcd9c690adb216cd70
e247442 <e247442@edu.sbl.ch>
2022-11-18 09:06:10 +0100

    Added items

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ if (ev.code === "Space") {
 
 const player = document.querySelector("#player")
 player.setAttribute("cx", 100*Math.random())
-player.setAttribute("cy", 100*Math.random())}
-
+player.setAttribute("cy", 100*Math.random())
+spawnItem()}
 else if (ev.key==="r"){
     player.setAttribute("fill", "red")
 }
 else if (ev.key==="g"){
     player.setAttribute("fill", "green")
 }
+
+}
+function spawnItem() {
+
+const NS = "http://www.w3.org/2000/svg"
+const item = document.createElementNS(NS , "circle")
+item.setAttribute("cx", 100 * Math.random())
+item.setAttribute("cy", 20 * Math.random() + 80)
+item.setAttribute("r", 2 )
+item.setAttribute("fill", "black")
+document.querySelector("svg").appendChild(item)
 
 }
 
